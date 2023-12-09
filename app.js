eb6d9f787670f0818ac2ccd995a9b22cad9f0bd0
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2023-12-09 17:06:48 -0500

    Update app.js

--- app.js
+++ app.js
@@ -301,7 +301,7 @@ clearTimeout(timeout)
 												 }, 5000)
 		if(daynumber === 6){
 			day.innerHTML = "Day " + daynumber + ". look for an abandoned building near (300, 700).";
-			model.style.transform = "translate3d(300px, 0px, 700px)";
+			model.style.transform = "translate3d(300px, 0px, -700px)";
 			model.style.position = "absolute";
 			model.style.height = "1000px";
 			model.style.rotate = "0 1 0 90deg";
