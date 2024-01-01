b1c58fb00a022a45d94c31e8b7f4e1defd2d44bd
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-01-01 12:58:53 -0500

    Add files via upload

--- replit.nix
+++ replit.nix
@@ -1,9 +1,6 @@
 { pkgs }: {
   deps = [
-		pkgs.nodePackages.prettier
-    pkgs.nodejs-16_x
-    pkgs.wget
     pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server
+    pkgs.nodePackages.typescript-language-server  
   ];
 }
