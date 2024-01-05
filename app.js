3f5213d8b0e713904b7bb3df3869c0604d2e51e4
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-01-05 13:56:05 -0500

    Update app.js

--- app.js
+++ app.js
@@ -360,7 +360,6 @@ clearTimeout(timeout)
 			model.style.position = "absolute";
 			model.style.height = "1000px";
 			model.style.width = "1000px";
-			document.getElementById("universe").style.perspective = "900px";
 			model.style.transform = "translate3d(-2000px, 0px, 700px) perspective(800px)";
 			task.max = 1;
 													 task.value = 1;
