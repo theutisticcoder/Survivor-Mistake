936c9f44f36509566a2d1e50499c016e8a34cd4a
The Paper <94148426+theutisticcoder@users.noreply.github.com>
2024-02-02 14:47:57 -0500

    Update app.js

--- app.js
+++ app.js
@@ -354,16 +354,15 @@ clearTimeout(timeout)
 												 }, 5000)
 		if(daynumber === 6){
 			day.innerHTML = "Day " + daynumber + ". look for an abandoned building near (2000, 700).";
-			model = new Image(400, 400);
 			
 			model.style.scale = "3 3 3";
-			
+			model.style.height = "500px";
+			model.style.width = "500px"
 			task.max = 1;
 			model.style.scale = "3 3 3";
 			model.src = "abandoned.png"
 			model.style.position = "absolute";
-			model.style.transform = "translate3d(-2000px, 0px, -700px) perspective(800px)";
-			document.getElementById("universe").appendChild(model);
+			model.style.transform = "translate3d(-2000px, 0px, 700px) perspective(800px)";
 			task.max = 1;
 													 task.value = 1;
 													 tasks.innerHTML = "Find Building";
